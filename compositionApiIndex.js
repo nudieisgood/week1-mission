68766ddaa218e69448a1fc5ab32413d8c80285d2
nudieisgood <nudieisgoodmii@gmail.com>
2023-12-30 12:58:34 +0800

    first commit

---
+++ compositionApiIndex.js
@@ -0,0 +1,22 @@
+import {
+  createApp,
+  ref,
+} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import { data } from "./data.js";
+
+const App = {
+  setup() {
+    const products = ref(data);
+    const product = ref(null);
+
+    const checkDetail = (id) => {
+      product.value = products.value.find((pro) => pro.id === id);
+      console.log(product.value);
+    };
+
+    return { products, product, checkDetail };
+  },
+};
+
+const app = createApp(App);
+app.mount("#app");
